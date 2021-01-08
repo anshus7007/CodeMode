bcbba989c02be02d73683cec63e794aedbc31aa2
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2021-01-08 20:01:36 +0530

    codechef

---
+++ src/com/example/Codeforces/CodeforcesRound695/A.java
@@ -0,0 +1,15 @@
+package com.example.Codeforces.CodeforcesRound695;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+
+        }
+    }
+
+}
---
+++ src/com/example/Codeforces/CodeforcesRound695/B.java
@@ -0,0 +1,14 @@
+package com.example.Codeforces.CodeforcesRound695;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+
+        }
+    }
+}
