f45a1d6d2356ece9aa50d0c023d27c342c5a2571
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2021-01-19 22:55:50 +0530

    codeforces

--- src/com/example/Codeforces/CodeforcesRound696/A.java
+++ src/com/example/Codeforces/CodeforcesRound696/A.java
@@ -5,6 +5,33 @@ import java.util.Scanner;
 class A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+            long n=sc.nextLong();
+            String s=sc.next();
+            StringBuilder w= new StringBuilder("1");
+            char a=s.charAt(0);
+            int sum=Character.getNumericValue(a)+Character.getNumericValue(w.charAt(0));
+            for(int i=1;i<n;i++)
+            {
+                char ch=s.charAt(i);
+                if(Character.getNumericValue(ch)+1 ==sum) {
+                    sum= Character.getNumericValue(ch);
+                    w.append("0");
+                }
+                else
+                {
+
+                    sum= Character.getNumericValue(ch)+1;
+
+                    w.append("1");
+                }
+
+            }
+            System.out.println(w.toString());
+
+        }
 
     }
 }
--- src/com/example/Codeforces/CodeforcesRound696/B.java
+++ src/com/example/Codeforces/CodeforcesRound696/B.java
@@ -2,9 +2,58 @@ package com.example.Codeforces.CodeforcesRound696;
 
 import java.util.Scanner;
 
-class B {
+public class B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0)
+        {
+            long n=sc.nextInt();
+            long a=nextPrime(n);
 
+            long b=nextPrime(a+n-1);
+            System.out.println(a*b);
+        }
+    }
+    static boolean isPrime(long n)
+    {
+        // Corner cases
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+
+        // This is checked so that we can skip
+        // middle five numbers in below loop
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (long i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        return true;
+    }
+
+    // Function to return the smallest
+    // prime number greater than N
+    static long nextPrime(long N)
+    {
+
+        // Base case
+        if (N <= 1)
+            return 2;
+
+        long prime = N;
+        boolean found = false;
+
+        // Loop continuously until isPrime returns
+        // true for a number greater than n
+        while (!found)
+        {
+            prime++;
+
+            if (isPrime(prime))
+                found = true;
+        }
+
+        return prime;
     }
 }
